cd0742224ef9b223a217a69e4b00b490fd92ff0f
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2023-12-21 21:37:24 -0800

    Create main.py

---
+++ day-11/part-01/main.py
@@ -0,0 +1,60 @@
+"""
+Advent of Code 2023 - Day 11, Part 1
+
+Task: Figure out the shortest distance of each pair of stars!
+"""
+
+import time
+import numpy as np
+from itertools import combinations
+
+
+def find_min_distance_of_all_pairs(file_path: str) -> int:
+    with open(file_path, "r", encoding="utf8") as file:
+        galaxy_map = []
+        stars = []
+
+        for i, line in enumerate(file.readlines()):
+            line = line.strip()
+            row = []
+
+            for j, char in enumerate(line):
+                if char == "#":
+                    stars.append((i, j))
+                    row.append(1)
+                else:
+                    row.append(0)
+
+            galaxy_map.append(row)
+
+        galaxy_map = np.array(galaxy_map)
+        empty_rows = [i for i in range(len(galaxy_map)) if sum(galaxy_map[i]) == 0]
+        empty_cols = [j for j in range(len(galaxy_map[0])) if sum(galaxy_map[:, j]) == 0]
+
+        sum_of_all_distance = 0
+
+        for star_i, star_j in combinations(stars, 2):
+            distance = 0
+
+            a, b = min(star_i[0], star_j[0]), max(star_i[0], star_j[0])
+            distance += sum(1 if i not in empty_rows else 2 for i in range(a + 1, b + 1, 1))
+
+            a, b = min(star_i[1], star_j[1]), max(star_i[1], star_j[1])
+            distance += sum(1 if j not in empty_cols else 2 for j in range(a + 1, b + 1, 1))
+
+            sum_of_all_distance += distance
+
+        return sum_of_all_distance
+
+
+if __name__ == "__main__":
+    start_time = time.time()  # Record the start time
+
+    input_file_path = "input.txt"
+    result = find_min_distance_of_all_pairs(input_file_path)
+
+    end_time = time.time()  # Record the end time
+    elapsed_time = end_time - start_time  # Calculate the elapsed time
+
+    print(f"The sum of all star distances is {result}")
+    print(f"Elapsed time: {elapsed_time:.6f} seconds")
